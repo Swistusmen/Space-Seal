3d13537f301b5c0b061b75faa95bb644225d9a2f
Swistusmen <michal76aw@gmail.com>
2021-10-23 10:25:36 +0200

    Working stream

--- SpaceSeal/src/org/freedesktop/gstreamer/tutorials/application/PipelineBuilder.java
+++ SpaceSeal/src/org/freedesktop/gstreamer/tutorials/application/PipelineBuilder.java
@@ -3,9 +3,13 @@ package org.freedesktop.gstreamer.tutorials.application;
 public class PipelineBuilder {
 
     static public String buildPipeline(String location){
-        String description="( filesrc location=" + location+" ! qtdemux name=d d. ! queue ! rtpgstpay ! pt=96 name=pay0 d. ! queue ! rtpmp4apay pt=97 name=pay1 )";
+        //String description=" filesrc location="+location + "  ! qtdemux " + "  ! aacparse " + "  ! decodebin " + "  ! audioconvert " + "  ! avenc_aac " + "  ! rtpmp4apay name=pay0"; //dziala- samo audio
+        //String description=" filesrc location=" +location+ "  ! qtdemux " + "  ! rtph264pay name=pay0"; //dziala wideo
 
-        //String description="( videotestsrc ! videoconvert ! x264enc ! rtph264pay name=pay0 pt=96 )";
+        String description=" filesrc location="+location + "  ! qtdemux name=d d." +
+                " ! queue "+"! rtph264pay name=pay0 d."
+                + " !queue ! aacparse " + "  ! decodebin " + "  ! audioconvert " + "  ! avenc_aac " + "  ! rtpmp4apay name=pay0";
+
         return description;
     }
 }
