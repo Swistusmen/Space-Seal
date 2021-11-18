69139d050f65c2bc826dc5aa877e74d4014504ef
Swistusmen <michal76aw@gmail.com>
2021-11-18 15:07:11 +0100

    Cleaning, screenshots

--- SpaceSeal/src/org/freedesktop/gstreamer/tutorials/application/MainActivity.kt
+++ SpaceSeal/src/org/freedesktop/gstreamer/tutorials/application/MainActivity.kt
@@ -17,9 +17,7 @@ class MainActivity : Activity() {
         val settingsButton=findViewById<Button>(R.id.SettingsButton)
         val recordButton=findViewById<Button>(R.id.RecordButton)
         val aboutButton=findViewById<Button>(R.id.AboutButton)
-        val requestButton=findViewById<Button>(R.id.HTTPButton)
         val requestTranscoding=findViewById<Button>(R.id.RequestTranscoding)
-        val sensorData=findViewById<Button>(R.id.SensorData)
 
         requestTranscoding.setOnClickListener{
             val settingsActivity=Intent(this, Transcoding::class.java)
@@ -43,15 +41,6 @@ class MainActivity : Activity() {
             startActivity(aboutActivity)
         }
 
-        requestButton.setOnClickListener{
-            val httpActivity= Intent(this,Request::class.java)
-            startActivity(httpActivity)
-        }
-
-        sensorData.setOnClickListener{
-            val sensorActivity= Intent(this, Sensor::class.java)
-            startActivity(sensorActivity)
-        }
 
 
     }
